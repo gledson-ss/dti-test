c8f97e4afc4ff484155f3829acb25d45615b8260
gledson-ss <github@gledson.dev>
2021-11-29 11:52:51 -0300

    feat: update api config
    n

---
+++ src/service/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import config from "../../config";
+
+export const api = axios.create({
+  baseURL: `https://www.omdbapi.com/?apikey=57b8c249`,
+  params: {
+    
+  }
+});
