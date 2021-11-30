4d9671ea1e11e755080871f82cbb48c887c09e68
gledson-ss <github@gledson.dev>
2021-11-29 22:03:12 -0300

    feat: add icon back

---
+++ src/components/atoms/BackIcon/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as Styled from "./styles";
+
+const BackIcon: React.FC = () => {
+  return <Styled.Container />;
+};
+
+export default BackIcon;
---
+++ src/components/atoms/BackIcon/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+
+export const Container = styled(Ionicons).attrs({
+  name: "arrow-back-outline",
+  size: 30,
+})``;
